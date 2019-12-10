71addc892cecd8da583379536499a81b55dee9f7
James Starkie <jamesstarkie@rideways.com>
2019-12-10 14:18:49 +0000

    Add linting and prettier

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,8 +1,6 @@
 <script context="module">
-	export function preload({ params, query }) {
-		return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
-			return { posts };
-		});
+	export function preload() {
+	  return this.fetch('blog.json').then((r) => r.json()).then((posts) => ({ posts }));
 	}
 </script>
 
