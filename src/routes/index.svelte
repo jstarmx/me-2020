d2eb7507d6d508ed691c82e593210c86053e0947
James Starkie <jstar.mx@gmail.com>
2020-05-02 10:50:21 +0100

    Update packages and apply prettier to project

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,48 @@
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+    h1,
+    figure,
+    p {
+        text-align: center;
+        margin: 0 auto;
+    }
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+    h1 {
+        font-size: 2.8em;
+        text-transform: uppercase;
+        font-weight: 700;
+        margin: 0 0 0.5em 0;
+    }
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+    figure {
+        margin: 0 0 1em 0;
+    }
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+    img {
+        width: 100%;
+        max-width: 400px;
+        margin: 0 0 1em 0;
+    }
 
-	p {
-		margin: 1em auto;
-	}
+    p {
+        margin: 1em auto;
+    }
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+    @media (min-width: 480px) {
+        h1 {
+            font-size: 4em;
+        }
+    }
 </style>
 
-<svelte:head>
-	<title>Sapper project template</title>
-</svelte:head>
-
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<div class="wrapper wrapper--home">
+    <header class="header">
+        <img class="logo logo--home" src="/images/logo2.svg" alt="" />
+        <h1>james starkie</h1>
+        <p class="header__intro">
+            front end dev / filmmaker / creative
+            <br />
+            manchester / uk
+        </p>
+    </header>
+    <footer class="footer" />
+</div>
