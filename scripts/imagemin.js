7e24528ae9fdd2c6b05b426843f0c973be8b68f6
GitLab CI <jamesstarkie@docabode.com>
2020-08-23 22:00:20 +0100

    Use export dir for imagemin processing

--- scripts/imagemin.js
+++ scripts/imagemin.js
@@ -4,7 +4,10 @@ const path = require('path');
 const imagemin = require('imagemin');
 const webp = require('imagemin-webp');
 
-const dir = path.resolve(__dirname, '../static/images/blog/how-a-browser-renders-a-web-page');
+const dir = path.resolve(
+    __dirname,
+    '../__sapper__/export/images/blog/how-a-browser-renders-a-web-page',
+);
 
 (async () => {
     try {
