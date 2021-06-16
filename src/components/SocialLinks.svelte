879cbb6d595aa1b4585d3073a1ac4bfb15697c32
James Starkie <jstar.mx@gmail.com>
2021-06-16 22:17:51 +0100

    Update social links

--- src/components/SocialLinks.svelte
+++ src/components/SocialLinks.svelte
@@ -22,12 +22,12 @@
 
 <ul>
     <li>
-        <a href="https://github.com/jstarmx" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/starkiedev" target="_blank" rel="noopener noreferrer">
             <img class="social__icon" src="/images/social-icons/github.svg" alt="Github" />
         </a>
     </li>
     <li>
-        <a href="https://twitter.com/jstarmx" target="_blank" rel="noopener noreferrer">
+        <a href="https://twitter.com/starkiedev" target="_blank" rel="noopener noreferrer">
             <img class="social__icon" src="/images/social-icons/twitter.svg" alt="Twitter" />
         </a>
     </li>
@@ -37,12 +37,7 @@
         </a>
     </li>
     <li>
-        <a href="https://instagram.com/jstarmx" target="_blank" rel="noopener noreferrer">
-            <img class="social__icon" src="/images/social-icons/instagram.svg" alt="Instagram" />
-        </a>
-    </li>
-    <li>
-        <a href="https://linkedin.com/in/jstarmx" target="_blank" rel="noopener noreferrer">
+        <a href="https://linkedin.com/in/starkiedev" target="_blank" rel="noopener noreferrer">
             <img class="social__icon" src="/images/social-icons/linkedin.svg" alt="LinkedIn" />
         </a>
     </li>
